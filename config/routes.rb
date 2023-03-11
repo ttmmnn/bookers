c52081883398eccfbdfa9c9f854c1f69ac0da549
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-11 02:35:25 +0000

    [Update]コントローラとルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/' => 'homes#top'
+  resources :books
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

f6183c843e01a1fbc2d30c9410f8b74d0feaa99b
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-12 01:05:18 +0000

    [update]indexビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book =Book.all
+  end
+
+  def create
+    @book =Book.new
+    book =Book.new(book_params)
+    book.save
+    redirect_to '/books/[:id]' #showのviewにリダイレクト
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end

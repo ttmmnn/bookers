5836f8f188570e12970ee9a313de6c756430c27e
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-13 06:44:39 +0000

    [Update]Updateとdestroy機能追加、バリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
+
   def index
-    @book =Book.all
+    @books =Book.all
+    @book =Book.new
   end
 
   def create
-    @book =Book.new
-    book =Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id),notice:'Book was successfully created.'
+    @book =Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:'Book was successfully created.'
+    else
+      @books =Book.all
+      render :index
+    end
   end
 
   def show
@@ -19,14 +24,17 @@ class BooksController < ApplicationController
   end
 
   def update
-    book =Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id),notice:'Book was successfully updated.'
+    @book =Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice:'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
     book =Book.find(params[:id])
-    book.destroy(book_params)
+    book.destroy
     redirect_to '/books'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
   resources :books
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books' => 'books#destroy', as: 'destroy_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
